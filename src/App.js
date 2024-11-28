bcca9a6462cdd0923810dcde3245730f9ec1591b
MarcosHBlanco <124410355+MarcosHBlanco@users.noreply.github.com>
2024-11-28 08:44:43 -0800

    added comments to the code

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
-	const [xIsNext, setXIsNext] = useState(true);
+	const [xIsNext, setXIsNext] = useState(true); //state defining next user's turn
 	const [squares, setSquares] = useState(Array(9).fill(null));
 
 	function handleClick(i) {
-		const nextSquares = squares.slice();
+		const nextSquares = squares.slice(); //creates a shallow copy of squares, that is going to be mutable instead of square
+		//check for square with content or a winner at all, and returns early if so
 		if (nextSquares[i] || calculateWinner(squares)) {
 			return;
 		}
+		//modifying state and taking turns
 		if (xIsNext) {
 			nextSquares[i] = "X";
 		} else {
@@ -28,7 +30,7 @@ export default function Board() {
 		console.log(nextSquares);
 	}
 	const winner = calculateWinner(squares);
-	let status;
+	let status; //displays winner of next player's turn
 	if (winner) {
 		status = "Winner: " + winner;
 	} else {
@@ -39,6 +41,7 @@ export default function Board() {
 			<div>
 				<p>{status}</p>
 			</div>
+			{/* game board */}
 			<div className="board-row">
 				<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 				<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -60,6 +63,7 @@ export default function Board() {
 
 function calculateWinner(squares) {
 	const lines = [
+		//possible winning combinations
 		[0, 1, 2],
 		[3, 4, 5],
 		[6, 7, 8],
@@ -70,6 +74,7 @@ function calculateWinner(squares) {
 		[2, 4, 6],
 	];
 	for (let i = 0; i < lines.length; i++) {
+		//loops through combinations to check for winner
 		const [a, b, c] = lines[i];
 		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
 			return squares[a];
